6fdebd5920bf7f3be6bce89c479997a20821c002
LMJW <heysuperming@gmail.com>
2019-03-12 21:40:10 +1100

    Update GameScene to draw hexnode and handle touch event

--- hex/Classes/GameScene.cpp
+++ hex/Classes/GameScene.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include "HexNode.h"
 #include "cocos2d.h"
+#include "math.h"
 
 USING_NS_CC;
 
@@ -18,23 +19,88 @@ bool GameScene::init() {
     if (!Layer::init()) {
         return false;
     }
-    drawHexes(4);
+    drawHexes(5);
+    auto touchListener = EventListenerTouchOneByOne::create();
+    touchListener->setSwallowTouches(true);
 
+    touchListener->onTouchBegan = CC_CALLBACK_2(GameScene::onTouchBegan, this);
+    touchListener->onTouchEnded = CC_CALLBACK_2(GameScene::onTouchEnded, this);
+    touchListener->onTouchMoved = CC_CALLBACK_2(GameScene::onTouchMoved, this);
+    touchListener->onTouchCancelled =
+        CC_CALLBACK_2(GameScene::onTouchCancelled, this);
+
+    _eventDispatcher->addEventListenerWithSceneGraphPriority(touchListener,
+                                                             this);
     return true;
 }
 
 /// @param n integer to determine the edge of hex grid. The total numbers of
 /// hexes in the grid equls to n*n;
 void GameScene::drawHexes(int n) {
+    /// hard code the 'center' of hex grid
     float init_x = this->getContentSize().width / 2;
     float init_y = this->getContentSize().height / 2;
-    float r = 30;
-    Color4F borderColor = Color4F::RED;
-    Color4F fillColor = Color4F::WHITE;
-    for (int i = -n / 2; i < n / 2; ++i) {
-        for (int j = -n / 2; j < n / 2; ++j) {
+    /// TODO: change the hard coded value
+    float r = this->r;
+
+    /// Current implementation will cause even number grid shift towards left
+    /// TODO: deal with even n;
+    for (int i = -n / 2; i < n * 1.0 / 2; ++i) {
+        for (int j = -n / 2; j < n * 1.0 / 2; ++j) {
             auto _drawNode = HexNode::createNode(init_x, init_y, r, i, j);
+            hexgrid.pushBack(_drawNode);
             this->addChild(_drawNode);
         }
     }
 }
+
+/// To find the hex index, we can do a vector transformation. Basically, we can
+/// convert the vector a * e_x + b * e_y to different coordinates as
+/// c * e_1 + d * e_2. Once we get the c & d, we can round the value to the hex
+/// coordinate.
+int GameScene::_hex_index(float x, float y) {
+    /// transformed vector coordinate
+    /// p, q represents the coordinate of hex coordinate
+    auto p = 2 * x / 3;
+    auto q = 1.732 / 3 * y - x / 3;
+
+    /// pidx/qidx represents the hex index of p/q coordinate
+    int pidx = 0;
+    int qidx = 0;
+
+    // aggregate the float number coordinate to hex index
+    auto f = [&](float i) {
+        i += r / 2;
+        if (i < 0) {
+            return i / r - 1;
+        }
+        return i / r;
+    };
+
+    pidx = f(p);
+    qidx = f(q);
+
+    /// Debug info
+    std::cout << p << " | " << q << std::endl;
+    std::cout << pidx << " | " << qidx << std::endl;
+}
+
+bool GameScene::onTouchBegan(cocos2d::Touch* touch, cocos2d::Event*) {
+    cocos2d::log("ontouchbegan");
+    auto touchpoint = touch->getLocationInView();
+    float init_x = this->getContentSize().width / 2;
+    float init_y = this->getContentSize().height / 2;
+
+    /// transform the touch coordinate
+    _hex_index(touchpoint.x - init_x, touchpoint.y - init_y);
+    return true;
+};
+void GameScene::onTouchEnded(cocos2d::Touch*, cocos2d::Event*) {
+    cocos2d::log("ontouchend");
+};
+void GameScene::onTouchMoved(cocos2d::Touch*, cocos2d::Event*) {
+    cocos2d::log("ontouchmoved");
+};
+void GameScene::onTouchCancelled(cocos2d::Touch*, cocos2d::Event*) {
+    cocos2d::log("ontouchcancelled");
+};
--- hex/Classes/GameScene.h
+++ hex/Classes/GameScene.h
@@ -10,12 +10,35 @@
 
 #include "cocos2d.h"
 
+/// GameScene is the scene that hex game is running in. This scene will
+/// initialize the hex grid, and touch event handler. To determine which hex is
+/// touched, we can use the x, y coordinate to compute out the hex index.
+
 class GameScene : public cocos2d::Layer {
+    cocos2d::Vector<cocos2d::DrawNode*> hexgrid;
+
 public:
     static cocos2d::Scene* createScene();
     virtual bool init();
-    CREATE_FUNC(GameScene);
+    /// Hex grid constant. r -> The edge length of hex.
+    /// The value can be reset by setting scene
+    float r = 30;
+
+    /// drawHexes method draw the @param by @param grid
     void drawHexes(int);
+
+    /// @param x, y indicate the x, y coordinate of the touch event. This
+    /// function will return the index of the hex.
+    int _hex_index(float x, float y);
+
+    /// event handler
+    virtual bool onTouchBegan(cocos2d::Touch*, cocos2d::Event*);
+    virtual void onTouchEnded(cocos2d::Touch*, cocos2d::Event*);
+    virtual void onTouchMoved(cocos2d::Touch*, cocos2d::Event*);
+    virtual void onTouchCancelled(cocos2d::Touch*, cocos2d::Event*);
+
+    /// Cocos2d provided macro function to avoid memory leak
+    CREATE_FUNC(GameScene);
 };
 
 #endif /* _GAMESCENE_H_ */
